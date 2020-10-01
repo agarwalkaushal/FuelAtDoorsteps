991c7bb64d484ba5e12f9e405a9bf50974abd85b
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2020-10-01 10:13:12 +0530

    orders screen with static data

--- src/Screens/Order/OrdersScreen.js
+++ src/Screens/Order/OrdersScreen.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, ActivityIndicator, Image, Text } from 'react-native';
+import { View, ActivityIndicator, Image, Text, FlatList, SectionList } from 'react-native';
+import { scale } from '../../Utils/responsiveScaling';
 import { Styles } from './Styles'
 
 class OrdersScreen extends React.Component {
@@ -8,31 +9,144 @@ class OrdersScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: false,
-            data: [],
+            loading: true,
+            data: [{
+                "id": 2,
+                "status": "Past",
+                "petrolQuantity": 15,
+                "dieselQuantity": 12,
+                "totalAmount": 1728.0,
+                "name": "kunal",
+                "email": "testing",
+                "order_id": "1234"
+            },
+            {
+                "id": 3,
+                "status": "Active",
+                "petrolQuantity": 15,
+                "dieselQuantity": 12,
+                "totalAmount": 1728.0,
+                "name": "kunal",
+                "email": "testing",
+                "order_id": "1234"
+            },
+            {
+                "id": 4,
+                "status": "Past",
+                "petrolQuantity": 15,
+                "dieselQuantity": 12,
+                "totalAmount": 1728.0,
+                "name": "kunal",
+                "email": "testing",
+                "order_id": "1234"
+            },
+            {
+                "id": 5,
+                "status": "Past",
+                "petrolQuantity": 15,
+                "dieselQuantity": 12,
+                "totalAmount": 1728.0,
+                "name": "kunal",
+                "email": "testing",
+                "order_id": "1234"
+            }],
+            formattedData: null,
         };
     }
 
+    componentDidMount() {
+        const { data } = this.state
+        let activeOrders = [], pastOrders = []
+
+        if (data.length > 0) {
+            data.forEach(element => {
+                if (element.status === 'Active') {
+                    activeOrders.push(element)
+                }
+                else {
+                    pastOrders.push(element)
+                }
+            });
+        }
+
+        const formattedData = [
+            {
+                title: 'ACTIVE ORDERS',
+                data: activeOrders,
+            },
+            {
+                title: 'PAST ORDERS',
+                data: pastOrders,
+            }
+        ]
+
+        this.setState({
+            formattedData: formattedData,
+            loading: false,
+        })
+    }
+
+
+
     render() {
-        const { loading, data } = this.state
+        const { loading, formattedData } = this.state
         return (
             <View style={Styles.screen}>
                 {loading ?
                     <ActivityIndicator style={Styles.loadingIndicator} color="#000000" /> :
                     <>
-                        {data.length === 0 ?
+                        {formattedData.length > 0 ?
+                            <SectionList
+                                sections={formattedData}
+                                renderSectionHeader={({ section: { title } }) => (
+                                    <View style={Styles.orderHeader}>
+                                        <Text style={Styles.orderText}>{title}</Text>
+                                    </View>
+                                )}
+                                renderItem={({ item }) => <Item data={item} />}
+                            /> :
                             <View style={Styles.emptyScreen}>
                                 <Image source={require('../../Images/fuel_empty.png')} style={Styles.emptyImage} />
                                 <Text style={Styles.emptyText}>Looks empty here</Text>
-                            </View> :
-                            <>
-                            </>
+                            </View>
+
                         }
                     </>
                 }
             </View>
         );
     }
+}
+
+function Item(props) {
+    return (
+        <View style={Styles.orderContainer}>
+            <View style={Styles.itemRow}>
+                <Text style={Styles.itemTitle}>Order ID</Text>
+                <Text style={Styles.itemValue}>{props.data.order_id}</Text>
+            </View>
+            <View style={Styles.itinerariesContainer}>
+                <Text style={Styles.itemTitle}>Itineraries</Text>
+                <View style={Styles.itemRow}>
+                    <Text style={Styles.itemSubTitle}>Petrol Qt.</Text>
+                    <Text style={Styles.itemValue}>{props.data.petrolQuantity}</Text>
+                </View>
+                <View style={Styles.itemRow}>
+                    <Text style={Styles.itemSubTitle}>Diesal Qt.</Text>
+                    <Text style={Styles.itemValue}>{props.data.dieselQuantity}</Text>
+                </View>
+            </View>
+            <View style={Styles.itemRow}>
+                <Text style={Styles.itemTitle}>Amount </Text>
+                <Text style={Styles.itemValue}>₹{props.data.totalAmount}/-</Text>
+            </View>
+            <View style={{ marginBottom: scale(7.5) }}></View>
+            <View style={Styles.itemRow}>
+                <Text style={Styles.itemTitle}>Placed on </Text>
+                <Text style={Styles.itemValue}>{String(new Date().getDate()) + '/' + String(new Date().getMonth() + 1) + '/' + String(new Date().getFullYear() + 1)}</Text>
+            </View>
+        </View>
+    );
 }
 
 export default OrdersScreen;
--- src/Screens/Order/Styles.js
+++ src/Screens/Order/Styles.js
@@ -36,5 +36,57 @@ export const Styles = StyleSheet.create(
             color: COLORS.primaryLight,
             alignSelf: 'center',
         },
+
+
+        orderHeader: {
+            height: verticalScale(45),
+            width: '100%',
+            backgroundColor: COLORS.offWhite,
+            justifyContent: 'center',
+        },
+
+        orderText: {
+            marginLeft: scale(10),
+            fontSize: scale(12),
+            fontWeight: 'bold',
+        },
+
+        orderContainer: {
+            marginHorizontal: scale(10),
+            marginVertical: verticalScale(10),
+            paddingHorizontal: scale(10),
+            paddingVertical: verticalScale(10),
+            borderWidth: scale(1),
+            borderColor: COLORS.primaryLight,
+            borderRadius: verticalScale(5),
+        },
+
+        itemRow: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            flexDirection: 'row',
+        },
+
+        itemTitle: {
+            fontSize: scale(14),
+        },
+
+        itemSubTitle: {
+            fontSize: scale(12),
+            fontWeight: 'bold',
+        },
+
+        itemValue: {
+            fontSize: scale(14),
+            fontWeight: 'bold',
+            textAlign: 'right',
+        },
+
+        itinerariesContainer: {
+            marginVertical: verticalScale(7.5),
+        },
+
+
     }
 );
